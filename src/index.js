16eb7ee0187c052d6808defc60948c0359f11db3
christian joseph <thirariichris@gmail.com>
2025-12-30 09:39:11 +0300

    added an event to the form and input to the webpage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskDescription = taskInput.value;
+
+    buildToDo(taskDescription);
+
+    form.reset();
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+  }
 });
